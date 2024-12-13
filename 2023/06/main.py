3ea67025b2e71ae0ffc585108e549d3f668fbf7e
Genesis <genesisrhapsodos98@gmail.com>
2024-12-13 23:44:18 +0700

    2023 Day 6 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/6#part2">[Continue to Part Two]</a></p></article>

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -3,8 +3,21 @@ import lib.aoc
 input_content = lib.aoc.get_current_input()
 lines = input_content.split('\n')
 
-s = 0
+times = [int(time) for time in lines[0].split(':')[1].split()]
+distances = [int(distance) for distance in lines[1].split(':')[1].split()]
+
+s = 1
 s2 = 0
+
+for time, distance in zip(times, distances):
+    ways = 0
+    for hold in range(time):
+        speed = hold
+        traveled = speed * (time - hold)
+        if traveled > distance:
+            ways += 1
+
+    s *= ways
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
