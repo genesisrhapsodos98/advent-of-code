dccd7d0421dd1ad2d99f78b96f6f6047473d9cbd
Genesis <genesisrhapsodos98@gmail.com>
2024-12-13 22:40:52 +0700

    2023 Day 3 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/3#part2">[Continue to Part Two]</a></p></article>

--- 2023/3/main.py
+++ 2023/3/main.py
@@ -1,10 +1,49 @@
+import collections
+
 import lib.aoc
+from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
-lines = input_content.split('\n')
+grid = FixedGrid.parse(input_content)
+
+symbol_positions = collections.defaultdict(set)
+numbers = []
 
 s = 0
 s2 = 0
 
+for y in grid.y_range:
+    num = 0
+    num_border = set()
+
+    for x in grid.x_range:
+        c = grid[x, y]
+        if c.isdigit():
+            if num == 0:
+                num_border |= {(x - 1, y - 1), (x - 1, y), (x - 1, y + 1)}
+
+            num = 10 * num + int(c)
+            num_border |= {(x, y - 1), (x, y + 1)}
+        else:
+            if num > 0:
+                # Add the border on the right
+                num_border |= {(x, y - 1), (x, y), (x, y + 1)}
+                numbers.append((num, num_border))
+                num = 0
+                num_border = set()
+            if c != '.':
+                symbol_positions[c].add((x, y))
+
+    if num > 0:
+        num_border |= {(grid.width, y - 1), (grid.width, y), (grid.width, y + 1)}
+        numbers.append((num, num_border))
+
+symbols = set()
+for places in symbol_positions.values():
+    symbols |=  places
+
+s = sum(n for n, border in numbers if len(border & symbols) > 0)
+
+
 lib.aoc.give_answer_current(1, s)
-lib.aoc.give_answer_current(2, s2)
+# lib.aoc.give_answer_current(2, s2)
