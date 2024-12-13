cda066559bd59e4e52e3fccd2555214c0e0669de
Genesis <genesisrhapsodos98@gmail.com>
2024-12-14 00:12:47 +0700

    2023 Day 7 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/7#part2">[Continue to Part Two]</a></p></article>

--- 2023/07/main.py
+++ 2023/07/main.py
@@ -1,10 +1,51 @@
+from collections import Counter
+
 import lib.aoc
 
 input_content = lib.aoc.get_current_input()
 lines = input_content.split('\n')
 
+cards = '23456789TJQKA'
+
+def strength(hand):
+    card_count = Counter()
+    high_cards = [cards.index(c) for c in hand]
+    for card in hand:
+        card_count[card] += 1
+
+    if 5 in card_count.values():
+        return 7, high_cards
+    if 4 in card_count.values():
+        return 6, high_cards
+    if 3 in card_count.values() and 2 in card_count.values():
+        return 5, high_cards
+    if 3 in card_count.values():
+        return 4, high_cards
+    if len(list(filter(lambda x: x == 2, card_count.values()))) == 2:
+        return 3, high_cards
+    if 2 in card_count.values():
+        return 2, high_cards
+    return 1, high_cards
+
+
+hands = []
+bids = []
+
+for line in lines:
+    hand, bid = line.split(' ')
+    hands.append(hand)
+    bids.append(int(bid))
+
 s = 0
 s2 = 0
+
+group = list(zip(hands, bids, [strength(h) for h in hands]))
+group.sort(key=lambda x: (x[2][0], x[2][1][0], x[2][1][1], x[2][1][2], x[2][1][3], x[2][1][4], ))
+print(group[0][0], group[0][2])
+print(group[-1][0], group[-1][2])
+
+for rank, (_, bid, (_, _)) in enumerate(group, 1):
+    s += rank * bid
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
