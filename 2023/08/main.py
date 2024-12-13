9a313b1bc3d3df9b4f07327e16e2b834bc0c58c9
Genesis <genesisrhapsodos98@gmail.com>
2024-12-14 01:17:03 +0700

    2023 Day 8 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/8#part2">[Continue to Part Two]</a></p></article>

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -1,10 +1,32 @@
+from collections import defaultdict
+
 import lib.aoc
 
 input_content = lib.aoc.get_current_input()
 lines = input_content.split('\n')
 
+instructions = lines[0]
+
+start = 'AAA'
+end = 'ZZZ'
+
+nodes = defaultdict(list)
+for line in lines[2:]:
+    node_data = line.split('=')
+    key = node_data[0].strip()
+    [l, r] = node_data[1].strip()[1:9].split(', ')
+    nodes[key] = [l, r]
+
 s = 0
 s2 = 0
+
+current = start
+while current != end:
+    instruction = instructions[s % len(instructions)]
+    [left, right] = nodes[current]
+    current = left if instruction == 'L' else right
+    s += 1
+
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
