fa4257f2e811c1b815cec6d83c96c8f78a27b821
Genesis <genesisrhapsodos98@gmail.com>
2024-12-13 21:12:43 +0700

    2023 Day 2 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations.</p><p>You have completed Day 2! You can <span class="share">[Share<span class="share-content">on

--- 2023/2/main.py
+++ 2023/2/main.py
@@ -27,12 +27,17 @@ for idx, line in enumerate(lines, 1):
     game = line.split(':')[1]
     sets = [x.strip() for x in game.split(';')]
     possible = True
+    max_r, max_g, max_b = 0, 0, 0
     for x in sets:
         r, g, b = parse_set(x)
+
+        max_r, max_g, max_b = max(max_r, r), max(max_g, g), max(max_b, b)
+
         if r > red or g > green or b > blue:
             possible = False
     if possible:
         s += idx
+    s2 += max_r * max_g * max_b
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
