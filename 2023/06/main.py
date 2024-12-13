809439e34a583cb2846b561c9a08139bcde069ae
Genesis <genesisrhapsodos98@gmail.com>
2024-12-13 23:51:34 +0700

    2023 Day 6 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations.</p><p>You have completed Day 6! You can <span class="share">[Share<span class="share-content">on

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -9,15 +9,29 @@ distances = [int(distance) for distance in lines[1].split(':')[1].split()]
 s = 1
 s2 = 0
 
-for time, distance in zip(times, distances):
-    ways = 0
-    for hold in range(time):
-        speed = hold
-        traveled = speed * (time - hold)
-        if traveled > distance:
-            ways += 1
+def solve(time, distance):
+    min = 0
+    max = time
 
-    s *= ways
+    travelled = 0
+    while travelled <= distance:
+        min += 1
+        travelled = min * (time - min)
+
+    travelled = 0
+    while travelled <= distance:
+        max -= 1
+        travelled = max * (time - max)
+
+    return max - min + 1
+
+for time, distance in zip(times, distances):
+    s *= solve(time, distance)
+
+time2 = int(''.join([str(time) for time in times]))
+distance2 = int(''.join([str(distance) for distance in distances]))
+
+s2 = solve(time2, distance2)
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
