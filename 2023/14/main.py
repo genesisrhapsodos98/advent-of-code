bc5757e63df678aea6f3d00a8a1bfb21b7404a4c
Genesis <genesisrhapsodos98@gmail.com>
2024-12-14 20:17:48 +0700

    2023 Day 14 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/14#part2">[Continue to Part Two]</a></p></article>

--- 2023/14/main.py
+++ 2023/14/main.py
@@ -1,10 +1,35 @@
 import lib.aoc
+from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
-lines = input_content.split('\n')
+grid = FixedGrid.parse(input_content)
+
+def tilt_grid(grid, direction):
+    dx, dy = direction
+    for coord, value in grid.items():
+        if value != 'O':
+            continue
+        x, y = coord
+        while (x + dx, y + dy) in grid:
+            next_coord = (x + dx, y + dy)
+            if grid[next_coord] in 'O#':
+                break
+            x, y = next_coord
+        grid[coord] = '.'
+        grid[x, y] = 'O'
+
+def score_grid(g):
+    score = 0
+    rocks = [(coord, value) for coord, value in g.items() if value == 'O']
+    for (x, y), _ in rocks:
+        score += grid.height - y
+    return score
 
 s = 0
 s2 = 0
 
+tilt_grid(grid, (0, -1))
+s += score_grid(grid)
+
 lib.aoc.give_answer_current(1, s)
-lib.aoc.give_answer_current(2, s2)
+# lib.aoc.give_answer_current(2, s2)
