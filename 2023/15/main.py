769dc6fe85e4f30197796eafdf6444d48f5e2d0c
Genesis <genesisrhapsodos98@gmail.com>
2024-12-15 00:50:27 +0700

    2023 Day 15 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/15#part2">[Continue to Part Two]</a></p></article>

--- 2023/15/main.py
+++ 2023/15/main.py
@@ -1,10 +1,21 @@
 import lib.aoc
 
 input_content = lib.aoc.get_current_input()
-lines = input_content.split('\n')
+segments = input_content.split(',')
+
+def hash_value(s: str):
+    cur = 0
+    for code in s.encode('ascii'):
+        cur += code
+        cur *= 17
+        cur %= 256
+    return cur
 
 s = 0
 s2 = 0
+
+for segment in segments:
+    s += hash_value(segment)
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
