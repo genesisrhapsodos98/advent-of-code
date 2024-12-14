772806e7b2b34db9fd31bbff0bbf934d4eb5fb98
Genesis <genesisrhapsodos98@gmail.com>
2024-12-14 10:22:54 +0700

    2023 Day 11 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/11#part2">[Continue to Part Two]</a></p></article>

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,10 +1,51 @@
 import lib.aoc
+from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
+
+# input_content = """...#......
+# .......#..
+# #.........
+# ..........
+# ......#...
+# .#........
+# .........#
+# ..........
+# .......#..
+# #...#....."""
+
 lines = input_content.split('\n')
+
+offset = 0
+for x in range(len(lines[0])):
+    col = [line[x + offset] for line in lines]
+    if '#' not in col:
+        for y in range(len(lines)):
+            lines[y] = lines[y][:x + offset] + '.' + lines[y][x + offset:]
+        offset += 1
+
+offset = 0
+for y in range(len(lines)):
+    row = lines[y + offset]
+    if '#' not in row:
+        lines.insert(y + offset, row)
+        offset += 1
+
+input_content = '\n'.join(lines)
+
+grid = FixedGrid.parse(input_content)
 
 s = 0
 s2 = 0
 
+galaxies = [coord for coord, value in grid.items() if value == '#']
+
+for i in range(len(galaxies) - 1):
+    for j in range(i, len(galaxies)):
+        ax, ay = galaxies[i]
+        bx, by = galaxies[j]
+
+        s += abs(bx - ax) + abs(by - ay)
+
 lib.aoc.give_answer_current(1, s)
-lib.aoc.give_answer_current(2, s2)
+# lib.aoc.give_answer_current(2, s2)
