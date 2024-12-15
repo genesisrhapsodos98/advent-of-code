5a4cb5384ae4882f4dc7418088f55f941fe11b0d
Genesis <genesisrhapsodos98@gmail.com>
2024-12-16 00:38:23 +0700

    2023 Day 18 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations.</p><p>You have completed Day 18! You can <span class="share">[Share<span class="share-content">on

--- 2023/18/main.py
+++ 2023/18/main.py
@@ -1,80 +1,117 @@
+from typing import Generator, Any
+
 import lib.aoc
-from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
-# input_content = """R 6 (#70c710)
-# D 5 (#0dc571)
-# L 2 (#5713f0)
-# D 2 (#d2c081)
-# R 2 (#59c680)
-# D 2 (#411b91)
-# L 5 (#8ceee2)
-# U 2 (#caa173)
-# L 1 (#1b58a2)
-# U 2 (#caa171)
-# R 2 (#7807d2)
-# U 3 (#a77fa3)
-# L 2 (#015232)
-# U 2 (#7a21e3)"""
 lines = input_content.split('\n')
 
-dig_plans = []
-directions = {
-    'U': (0, -1),
-    'D': (0, 1),
-    'L': (-1, 0),
-    'R': (1, 0),
-}
+def parse_input(input_str: str):
+    for line in input_str.splitlines():
+        a, b, c = line.split()
+        yield a, int(b)
 
-for line in lines:
-    a, b, c = line.split()
-    dig_plans.append((a, int(b), c))
+def parse_input_2(input_str: str):
+    for line in input_str.splitlines():
+        _, _, c = line.split()
+        num = int(c[2:-1], base=16)
+        yield 'RDLU'[num&0xF], num // 16
 
-s = 0
-s2 = 0
+def solve(sequence: Generator[tuple[str, int], Any, None]):
+    x, y = 0, 0
 
-current_pos = (0, 0)
-visited_pos = [(0, 0)]
+    corners = [(x, y)]
 
-for plan in dig_plans:
-    d, length, hex_code = plan
-    dx, dy = directions[d]
-    for _ in range(length):
-        x, y = current_pos
-        current_pos = x + dx, y + dy
-        visited_pos.append(current_pos)
+    x_points = set()
+    y_points = set()
 
-min_x = min([x for x, y in visited_pos])
-min_y = min([y for x, y in visited_pos])
-
-transposed_pos = [(x - min_x, y - min_y) for x, y in visited_pos]
-grid_dict = dict((pos, '#') for pos in transposed_pos)
-
-grid = FixedGrid.from_dict(grid_dict, missing='.')
-
-seen = set(transposed_pos)
-fill = set(seen)
-
-s = 0
-s2 = 0
-
-for y in grid.y_range:
-    is_enclosed = False
-    for x in grid.x_range:
-        if (x, y) in seen:
-            if (x, y - 1) in seen and (x, y + 1) in seen:
-                is_enclosed = not is_enclosed
-            elif (x, y + 1) in seen and (x + 1, y) in seen:
-                is_enclosed = not is_enclosed
-            elif (x, y + 1) in seen and (x - 1, y) in seen:
-                is_enclosed = not is_enclosed
+    for d, num in sequence:
+        if d == 'U':
+            y -= num
+        elif d == 'D':
+            y += num
+        elif d == 'R':
+            x += num
         else:
-            if is_enclosed:
-                fill.add((x, y))
+            x -= num
 
-for coord in fill:
+        corners.append((x, y))
+        x_points.add(x)
+        y_points.add(y)
 
-s += sum([1 for _, cell in grid.items() if cell == '#'])
+    x_points = sorted(x_points)
+    y_points = sorted(y_points)
+
+    compression = {}
+
+    grid_sizes = {}
+
+    new_x = 0
+
+    for ix, x in enumerate(x_points):
+        new_y = 0
+        for iy, y in enumerate(y_points):
+            compression[x, y] = (new_x, new_y)
+
+            grid_sizes[new_x, new_y] = 1
+
+            if ix > 0:
+                last_x = x_points[ix - 1]
+                grid_sizes[new_x - 1, new_y] = (x - last_x - 1)
+                if iy > 0:
+                    last_y = y_points[iy - 1]
+                    grid_sizes[new_x - 1, new_y - 1] = (x - last_x - 1) * (y - last_y - 1)
+            if iy > 0:
+                last_y = y_points[iy - 1]
+                grid_sizes[new_x, new_y - 1] = (y - last_y - 1)
+
+            new_y += 2
+
+        new_x += 2
+
+    seen = set()
+
+    for (e0, e1) in zip(corners, corners[1:] + [corners[0]]):
+        x0, y0 = compression[e0]
+        x1, y1 = compression[e1]
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
+
+        for x in range(x0, x1 + 1):
+            for y in range(y0, y1 + 1):
+                seen.add((x, y))
+
+    min_x = min(x for x, y in seen)
+    max_x = max(x for x, y in seen)
+    min_y = min(y for x, y in seen)
+    max_y = max(y for x, y in seen)
+
+    x_range = range(min_x, max_x + 1)
+    y_range = range(min_y, max_y + 1)
+
+    fill = set(seen)
+
+    for y in y_range:
+        is_enclosed = False
+        for x in x_range:
+            if (x, y) in seen:
+                if (x, y - 1) in seen and (x, y + 1) in seen:
+                    is_enclosed = not is_enclosed
+                elif (x, y + 1) in seen and (x + 1, y) in seen:
+                    is_enclosed = not is_enclosed
+                elif (x, y + 1) in seen and (x - 1, y) in seen:
+                    is_enclosed = not is_enclosed
+            else:
+                if is_enclosed:
+                    fill.add((x, y))
+
+    return sum(map(grid_sizes.__getitem__, fill))
+
+s = solve(parse_input(input_content))
+s2 = solve(parse_input_2(input_content))
+
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
