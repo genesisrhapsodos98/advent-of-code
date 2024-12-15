023ace489732da8c7d0ff8934fadf69143985792
Genesis <genesisrhapsodos98@gmail.com>
2024-12-15 12:22:18 +0700

    2024 Day 15 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to finding the Chief Historian. <a href="/2024/day/15#part2">[Continue to Part Two]</a></p></article>

--- 2023/17/main.py
+++ 2023/17/main.py
@@ -1,7 +1,49 @@
 import lib.aoc
+from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
-lines = input_content.split('\n')
+input_content = \
+"""2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"""
+grid = FixedGrid.parse(input_content, value_fn=int)
+
+def custom_dfs(pos, dir, dir_count, path, value, visited):
+    x, y = pos
+    dx, dy = dir
+
+    if (x, y) == (grid.width -1, grid.height - 1):
+        return path, value
+
+
+    next_nodes = grid.neighbors(x, y)
+    # Remove visited
+    next_nodes = [node for node in next_nodes if node not in visited]
+    # Remove backwards
+    next_nodes = [node for node in next_nodes if node != (x - dx, y - dy)]
+    # Remove 4-length paths
+    next_nodes = [node for node in next_nodes if node != (x + dx, y + dy) or dir_count == 3]
+
+    next_values = []
+    for node in next_nodes:
+        nx, ny = node
+        ndx, ndy = nx - x, ny - y
+        new_dir_count = 0 if (ndx, ndy) != (dx, dy) else dir_count + 1
+        new_path = path
+        new_path.append(node)
+        visited.add(node)
+        new_value = value + grid[node]
+        next_values.append(custom_dfs((nx, ny), (ndx, ndy), new_dir_count, new_path, new_value, visited))
 
 s = 0
 s2 = 0
--- 2024/15/main.py
+++ 2024/15/main.py
@@ -1,10 +1,97 @@
 import lib.aoc
+from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
-lines = input_content.split('\n')
+# input_content = \
+# """##########
+# #..O..O.O#
+# #......O.#
+# #.OO..O.O#
+# #..O@..O.#
+# #O#..O...#
+# #O..O..O.#
+# #.OO.O.OO#
+# #....O...#
+# ##########
+#
+# <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+# vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+# ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+# <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+# ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+# ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+# >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+# <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+# ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+# v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
+
+# input_content = \
+# """########
+# #..O.O.#
+# ##@.O..#
+# #...O..#
+# #.#.O..#
+# #...O..#
+# #......#
+# ########
+#
+# <^^>>>vv<v>>v<<"""
+
+grid_content, movements_content = input_content.split('\n\n')
+movements = ''.join(movements_content.split('\n'))
+grid = FixedGrid.parse(grid_content)
 
 s = 0
 s2 = 0
 
+x, y = grid.find("@")
+grid[x, y] = '.'
+moves = {
+    '<': (-1, 0),
+    '^': (0, -1),
+    '>': (1, 0),
+    'v': (0, 1)
+}
+
+for move in movements:
+    # grid.print()
+    dx, dy = moves[move]
+    nx, ny = x + dx, y + dy
+
+    if (nx, ny) not in grid or grid[nx, ny] == '#':
+        continue
+
+    if grid[nx, ny] == '.':
+        x, y = nx, ny
+        continue
+
+    if grid[nx, ny] == 'O':
+        px, py = nx, ny
+        while grid[px, py] == 'O':
+            px, py = px + dx, py + dy
+
+        if grid[px, py] == '#':
+            continue
+
+        while (px, py) != (nx, ny):
+            grid[px, py] = 'O'
+            px, py = px - dx, py - dy
+
+        grid[nx, ny] = '.'
+        x, y = nx, ny
+
+def eval_grid(grid):
+    boxes_pos = [coord for coord, value in grid.items() if value == 'O']
+    answer = 0
+    for x, y in boxes_pos:
+        answer += 100 * y +  x
+    return answer
+
+s = eval_grid(grid)
+grid.print()
+
+# assert s == 10092, f'{s} is not 10092'
+# assert s == 2028, f'{s} is not 2028'
+
 lib.aoc.give_answer_current(1, s)
-lib.aoc.give_answer_current(2, s2)
+# lib.aoc.give_answer_current(2, s2)
