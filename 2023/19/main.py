f5d57e6d7dafe07e41cb0570767e85fc7fb865da
Genesis <genesisrhapsodos98@gmail.com>
2024-12-16 01:49:17 +0700

    Add setup script

--- 2023/19/main.py
+++ 2023/19/main.py
@@ -1,10 +1,30 @@
+import collections
+import functools
+import itertools
+import math
+import re
 import lib.aoc
+import lib.graph
+import lib.grid
 
-input_content = lib.aoc.get_current_input()
-lines = input_content.split('\n')
+def part1(s):
+##    nums = list(map(lambda r:r[0], parse.findall('{:d}', s)))
+##    lines = s.splitlines()
+##    groups = s.split('\n\n')
+##    grid = lib.grid.FixedGrid.parse(s, value_fn=int)
+##    grid = lib.grid.FixedGrid.parse(s,
+##                                    linesplit_fn=lambda line: line.split(),
+##                                    value_fn=int)
+    print(f'The answer to part one is {answer}')
+    if input('Submit answer? ').lower() in ('y', 'yes', '1'):
+        assert(lib.aoc.give_answer_current(1, answer))
 
-s = 0
-s2 = 0
+def part2(s):
+    pass
+##    print(f'The answer to part two is {answer}')
+##    if input('Submit answer? ').lower() in ('y', 'yes', '1'):
+##        assert(lib.aoc.give_answer_current(2, answer))
 
-lib.aoc.give_answer_current(1, s)
-lib.aoc.give_answer_current(2, s2)
+INPUT = lib.aoc.get_current_input()
+part1(INPUT)
+part2(INPUT)
