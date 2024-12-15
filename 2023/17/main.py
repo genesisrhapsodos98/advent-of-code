de545645746be34e073617f5c35c4bee74ef1ee6
Genesis <genesisrhapsodos98@gmail.com>
2024-12-15 14:06:33 +0700

    2023 Day 17 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to restoring snow operations. <a href="/2023/day/17#part2">[Continue to Part Two]</a></p></article>

--- 2023/17/main.py
+++ 2023/17/main.py
@@ -1,52 +1,76 @@
 import lib.aoc
+from lib.graph import make_lazy_graph, all_reachable, dijkstra_length_fuzzy_end
 from lib.grid import FixedGrid
 
 input_content = lib.aoc.get_current_input()
-input_content = \
-"""2413432311323
-3215453535623
-3255245654254
-3446585845452
-4546657867536
-1438598798454
-4457876987766
-3637877979653
-4654967986887
-4564679986453
-1224686865563
-2546548887735
-4322674655533"""
 grid = FixedGrid.parse(input_content, value_fn=int)
 
-def custom_dfs(pos, dir, dir_count, path, value, visited):
-    x, y = pos
-    dx, dy = dir
+graph = {}
+for coord, value in grid.items():
+    x, y = coord
+    neighbors = grid.neighbors(x, y)
+    graph[coord] = [(n_coord, grid[n_coord]) for n_coord in neighbors]
 
-    if (x, y) == (grid.width -1, grid.height - 1):
-        return path, value
+def generate_heuristic_to_end_node(grid: FixedGrid):
+    def heuristic_neighbor(state):
+        x, y = state
+        for n in grid.neighbors(x, y):
+            yield n, grid[x, y]
+
+    heuristic_graph = make_lazy_graph(heuristic_neighbor)
+
+    heuristic = dict(all_reachable(heuristic_graph,
+                                             (grid.width-1,
+                                              grid.height-1)))
+    heuristic[grid.width-1, grid.height-1] = 0
+    return heuristic.__getitem__
+
+def solve(grid: FixedGrid, min_steps = 0, max_steps = 3):
+    w, h = grid.width, grid.height
+
+    def neighbor_fn(state):
+        x, y, horizontal = state
+        if horizontal:
+            for ndy in (1, -1):
+                ny = y
+                cost = 0
+                for steps in range(1, max_steps + 1):
+                    ny += ndy
+                    if 0 <= ny < h:
+                        cost += grid[x, ny]
+                        if steps >= min_steps:
+                            new_state = (x, ny, False)
+                            yield new_state, cost
+                    else:
+                        break
+        else:
+            for ndx in (1, -1):
+                nx = x
+                cost = 0
+                for steps in range(1, max_steps + 1):
+                    nx += ndx
+                    if 0 <= nx < w:
+                        cost += grid[nx, y]
+                        if steps >= min_steps:
+                            new_state = (nx, y, True)
+                            yield new_state, cost
+
+    def end_fn(state):
+        x, y, _ = state
+        return x == w - 1 and y == h - 1
+
+    graph = make_lazy_graph(neighbor_fn)
+    heuristic_to_end = generate_heuristic_to_end_node(grid)
+
+    def heuristic(state):
+        x, y, _ = state
+        return heuristic_to_end((x, y))
+
+    return dijkstra_length_fuzzy_end(graph, [(0, 0, True), (0, 0, False)], end_fn, heuristic)
 
 
-    next_nodes = grid.neighbors(x, y)
-    # Remove visited
-    next_nodes = [node for node in next_nodes if node not in visited]
-    # Remove backwards
-    next_nodes = [node for node in next_nodes if node != (x - dx, y - dy)]
-    # Remove 4-length paths
-    next_nodes = [node for node in next_nodes if node != (x + dx, y + dy) or dir_count == 3]
-
-    next_values = []
-    for node in next_nodes:
-        nx, ny = node
-        ndx, ndy = nx - x, ny - y
-        new_dir_count = 0 if (ndx, ndy) != (dx, dy) else dir_count + 1
-        new_path = path
-        new_path.append(node)
-        visited.add(node)
-        new_value = value + grid[node]
-        next_values.append(custom_dfs((nx, ny), (ndx, ndy), new_dir_count, new_path, new_value, visited))
-
-s = 0
-s2 = 0
+s = solve(grid)
+s2 = solve(grid, 4, 10)
 
 lib.aoc.give_answer_current(1, s)
 lib.aoc.give_answer_current(2, s2)
