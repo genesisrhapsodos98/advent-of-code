031f6ff5df8f7f548ed4153a008a7aabcc18fe71
Ly Gia Lap <lap.ly@elca.vn>
2024-12-18 12:30:02 +0700

    2024 Day 18 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to finding the Chief Historian.</p><p>You have completed Day 18! You can <span class="share">[Share<span class="share-content">on

--- 2024/18/main.py
+++ 2024/18/main.py
@@ -7,17 +7,17 @@ import lib.aoc
 import lib.graph
 import lib.grid
 
-def parse_input(s, lim=0):
+def parse_input(s, lim=0, size=70):
     lines = s.splitlines()
     d = dict()
     d[(0, 0)] = '.'
+    d[(size, size)] = '.'
     for idx, line in enumerate(lines):
         if idx == lim:
             break
         x, y = list(map(int, line.split(',')))
         d[(x, y)] = '#'
     grid = lib.grid.FixedGrid.from_dict(d, missing='.')
-    grid.print(line_spacing='')
     return grid
 
 def shortest_length(grid: lib.grid.FixedGrid):
@@ -44,8 +44,21 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-##    lib.aoc.give_answer_current(2, answer)
+    lines = s.splitlines()
+    i = 2096
+    can_escape = True
+    while can_escape:
+        print(f'Testing {i}')
+        grid = parse_input(s, i)
+        escape_path = shortest_length(grid)
+        if escape_path == -1:
+            can_escape = False
+        else:
+            i += 1
+
+    answer = lines[i - 1]
+
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
