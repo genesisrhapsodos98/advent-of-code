f3c32c5e4c3a23905c73df8b7efbb4e383135afd
Genesis <genesisrhapsodos98@gmail.com>
2024-12-21 16:57:00 +0700

    More intuitive variable names

--- 2024/21/main.py
+++ 2024/21/main.py
@@ -10,34 +10,34 @@ from lib.graphics import *
 
 visited = dict()
 
-def cheapest_arrow_pad(curr, curc, destr, destc, n_robots):
+def cheapest_arrow_pad(cur_row, cur_col, dest_row, dest_col, n_robots):
     answer = math.inf
 
-    state = (curr, curc, destr, destc, n_robots)
+    state = (cur_row, cur_col, dest_row, dest_col, n_robots)
 
     if state in visited:
-        return visited[(curr, curc, destr, destc, n_robots)]
+        return visited[(cur_row, cur_col, dest_row, dest_col, n_robots)]
 
-    queue = [(curr, curc, '')]
+    queue = [(cur_row, cur_col, '')]
     while queue:
-        r, c, presses = queue.pop(0)
-        if r == destr and c == destc:
+        row, col, presses = queue.pop(0)
+        if row == dest_row and col == dest_col:
             rec = cheapest_robot(presses + 'A', n_robots - 1)
             answer = min(answer, rec)
             continue
 
-        if r == 0 and c == 0:
+        if row == 0 and col == 0:
             continue
 
         else:
-            if r < destr:
-                queue.append((r + 1, c, presses + 'v'))
-            elif r > destr:
-                queue.append((r -1, c, presses + '^'))
-            if c < destc:
-                queue.append((r, c + 1, presses + '>'))
-            elif c > destc:
-                queue.append((r, c - 1, presses + '<'))
+            if row < dest_row:
+                queue.append((row + 1, col, presses + 'v'))
+            elif row > dest_row:
+                queue.append((row -1, col, presses + '^'))
+            if col < dest_col:
+                queue.append((row, col + 1, presses + '>'))
+            elif col > dest_col:
+                queue.append((row, col - 1, presses + '<'))
 
     visited[state] = answer
     return answer
@@ -49,66 +49,66 @@ def cheapest_robot(presses, n_robots):
     result = 0
     pad_config = 'X^A<v>'
 
-    curr = 0
-    curc = 2
+    cur_row = 0
+    cur_col = 2
 
     for i in range(len(presses)):
-        for nextr in range(0, 2):
-            for nextc in range(0, 3):
-                if pad_config[nextr * 3 + nextc] == presses[i]:
-                    result += cheapest_arrow_pad(curr, curc, nextr, nextc, n_robots)
-                    curr = nextr
-                    curc = nextc
+        for next_row in range(0, 2):
+            for next_col in range(0, 3):
+                if pad_config[next_row * 3 + next_col] == presses[i]:
+                    result += cheapest_arrow_pad(cur_row, cur_col, next_row, next_col, n_robots)
+                    cur_row = next_row
+                    cur_col = next_col
 
     return result
 
-def cheapest(curr, curc, destr, destc, n_robots):
+def cheapest(cur_row, cur_col, dest_row, dest_col, n_robots):
     answer = math.inf
-    queue = [(curr, curc, '')]
+    queue = [(cur_row, cur_col, '')]
 
     while queue:
-        r, c, presses = queue.pop(0)
-        if r == destr and c == destc:
+        row, col, presses = queue.pop(0)
+        if row == dest_row and col == dest_col:
             rec = cheapest_robot(presses + 'A', n_robots)
             answer = min(answer, rec)
             continue
 
-        if r == 3 and c == 0:
+        if row == 3 and col == 0:
             continue
 
         else:
-            if r < destr:
-                queue.append((r + 1, c, presses + 'v'))
-            elif r > destr:
-                queue.append((r - 1, c, presses + '^'))
-            if c < destc:
-                queue.append((r, c + 1, presses + '>'))
-            elif c > destc:
-                queue.append((r, c - 1, presses + '<'))
+            if row < dest_row:
+                queue.append((row + 1, col, presses + 'v'))
+            elif row > dest_row:
+                queue.append((row - 1, col, presses + '^'))
+            if col < dest_col:
+                queue.append((row, col + 1, presses + '>'))
+            elif col > dest_col:
+                queue.append((row, col - 1, presses + '<'))
 
 
     return answer
 
 def solve(codes, n_robots):
-    sum = 0
+    total = 0
     pad_config = '789456123X0A'
 
-    curr = 3
-    curc = 2
+    cur_row = 3
+    cur_col = 2
 
     for code in codes:
         result = 0
         for i in range(len(code)):
-            for nextr in range(0, 4):
-                for nextc in range(0, 3):
-                    if pad_config[nextr * 3 + nextc] == code[i]:
-                        result += cheapest(curr, curc, nextr, nextc, n_robots)
-                        curr = nextr
-                        curc = nextc
+            for next_row in range(0, 4):
+                for next_col in range(0, 3):
+                    if pad_config[next_row * 3 + next_col] == code[i]:
+                        result += cheapest(cur_row, cur_col, next_row, next_col, n_robots)
+                        cur_row = next_row
+                        cur_col = next_col
 
-        sum += result * int(code[:-1])
+        total += result * int(code[:-1])
 
-    return sum
+    return total
 
 
 
