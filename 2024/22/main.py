82a46503d94f8326224ef17fd6306cc7d4a1bfa6
Genesis <genesisrhapsodos98@gmail.com>
2024-12-22 12:31:48 +0700

    2024 Day 22 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to finding the Chief Historian. You got rank 912 on <a href="/2024/leaderboard/day/22">this star's leaderboard</a>.</p><p>You have completed Day 22! You can <span class="share">[Share<span class="share-content">on

--- 2024/22/main.py
+++ 2024/22/main.py
@@ -13,12 +13,15 @@ def parse_input(s):
     numbers = list(map(int, lines))
     return numbers
 
+@functools.cache
 def mix(num, val):
     return val ^ num
 
+@functools.cache
 def prune(num):
     return num % 16777216
 
+@functools.cache
 def randomize(seed):
     result = prune(mix(seed, seed * 64))
     result = prune(mix(result, result // 32))
@@ -37,10 +40,43 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
+    numbers = parse_input(s)
+    prices = []
+    differences = []
     answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    occurrences = []
+    all_occurrences = set()
+
+    for idx, num in enumerate(numbers):
+        final = num
+        prices.append([num % 10])
+        differences.append([None])
+        occurrences.append(dict())
+        for i in range(2000):
+            final = randomize(final)
+            new_price = final % 10
+            prices[idx].append(new_price)
+            differences[idx].append(new_price - prices[idx][-2])
+            if len(differences[idx]) > 4:
+                key = (differences[idx][-4], differences[idx][-3], differences[idx][-2], differences[idx][-1])
+                all_occurrences.add(key)
+                if key not in occurrences[idx]:
+                    occurrences[idx][key] = new_price
+
+    best_value = 0
+    for occ in all_occurrences:
+        occ_value = 0
+        for idx, num in enumerate(numbers):
+            if occ not in occurrences[idx]:
+                continue
+            first_occurrence_price = occurrences[idx][occ]
+            occ_value += first_occurrence_price
+        if occ_value > best_value:
+            best_value = occ_value
+
+    answer = best_value
+
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
