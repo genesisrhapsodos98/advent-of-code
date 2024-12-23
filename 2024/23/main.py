177dfedadec6c9035cbfccc95dffc417fed0c008
Genesis <genesisrhapsodos98@gmail.com>
2024-12-23 12:39:43 +0700

    2024 Day 23 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to finding the Chief Historian.</p><p>You have completed Day 23! You can <span class="share">[Share<span class="share-content">on

--- 2024/23/main.py
+++ 2024/23/main.py
@@ -3,6 +3,9 @@ import functools
 import itertools
 import math
 import re
+from collections import defaultdict
+from itertools import product
+
 import lib.aoc
 import lib.graph
 import lib.grid
@@ -12,32 +15,33 @@ import networkx as nx
 def parse_input(s):
     lines = s.splitlines()
     pairs = []
+
     for line in lines:
         a, b = line.split('-')
         pairs.append((a, b))
-    return pairs
 
-def part1(s):
-    pairs = parse_input(s)
-    graph = nx.Graph()
+    computers = nx.Graph()
     for pair in pairs:
         left, right = pair
-        graph.add_edge(left, right)
-    graph = graph.to_undirected()
-    cycles = nx.simple_cycles(graph, length_bound=3)
+        computers.add_edge(left, right)
+    computers = computers.to_undirected()
+
+    return computers
+
+def part1(s):
+    computers = parse_input(s)
+    cycles = nx.simple_cycles(computers, length_bound=3)
     valid_cycles = [cycle for cycle in cycles if len(cycle) == 3 and any([edge.startswith('t') for edge in cycle])]
     answer = len(valid_cycles)
-    valid_cycles_set = set([tuple(cycle) for cycle in valid_cycles])
-    answer_2 = len(valid_cycles_set)
-    print(answer)
-    print(answer_2)
+
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
-    answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    computers = parse_input(s)
+    all_cliques = list(nx.enumerate_all_cliques(computers))
+    sorted_computers = sorted(all_cliques[-1])
+    answer = ','.join(sorted_computers)
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
