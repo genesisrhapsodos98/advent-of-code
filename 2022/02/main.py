373137d299716ef3c6660d2fb084d1713d4d4966
Ly Gia Lap <lap.ly@elca.vn>
2025-11-26 11:19:12 +0700

    2022 Day 2 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit.</p><p>You have completed Day 2! You can <span class="share">[Share<span class="share-content">on

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -20,6 +20,20 @@ def resolve_match(match):
     result_score = {0: 3, 1: 6, 2: 0}[(player_choice - opp_choice) % 3]
     return player_score + result_score
 
+def resolve_instruction(match):
+    opp, instruction = match.split()
+    opp_choice = 'ABC'.index(opp)
+    result_score = {'X': 0, 'Y': 3, 'Z': 6}[instruction]
+    if instruction == 'X':
+        player_choice = (opp_choice + 2) % 3
+    elif instruction == 'Y':
+        player_choice = opp_choice
+    else:
+        player_choice = (opp_choice + 1) % 3
+    player_score = player_choice + 1
+    return result_score + player_score
+
+
 def part1(s):
     matches = parse_input(s)
     answer = 0
@@ -28,10 +42,11 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
+    matches = parse_input(s)
     answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    for match in matches:
+        answer += resolve_instruction(match)
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
