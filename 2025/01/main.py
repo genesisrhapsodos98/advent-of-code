30f228237d64753f7118b92c4f17434fb6146384
Ly Gia Lap <lap.ly@elca.vn>
2025-12-01 12:28:08 +0700

    2025 Day 1 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to decorating the North Pole.</p><p>You have completed Day 1! You can <span class="share">[Share<span class="share-content">on

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -1,12 +1,4 @@
-import collections
-import functools
-import itertools
-import math
-import re
 import lib.aoc
-import lib.graph
-import lib.grid
-from lib.graphics import *
 
 def parse_input(s):
     lines = s.splitlines()
@@ -22,19 +14,31 @@ def part1(s):
     current = 50
     answer = 0
     for (d, a) in instructions:
-        if d == 'L':
-            current -= a
-        else:
-            current += a
-        current = current % 100
+        delta = a if d == 'R' else -a
+        previous = current
+        end = previous + delta
+        current = end % 100
         if current == 0:
             answer += 1
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
+    instructions = parse_input(s)
+    current = 50
     answer = 0
+    for (d, a) in instructions:
+        delta = a if d == 'R' else -a
+        previous = current
+        end = previous + delta
+
+        if delta > 0:
+            times = (end // 100) - (previous // 100)
+        else:
+            times = ((previous - 1) // 100) - ((end - 1) // 100)
+
+        answer += times
+        current = end % 100
+
     lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
