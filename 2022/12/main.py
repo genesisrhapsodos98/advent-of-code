65f9f0e66b49ee737448447e3ea32d8a4f89b217
Ly Gia Lap <lap.ly@elca.vn>
2025-12-02 11:57:57 +0700

    2022 Day 12 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit.</p><p>You have completed Day 12! You can <span class="share">[Share<span class="share-content">on

--- 2022/12/main.py
+++ 2022/12/main.py
@@ -35,10 +35,22 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
-    answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    grid = parse_input(s)
+
+    def neighbor_fn(pos):
+        start_elevation = resolve_elevation(grid[pos])
+        for n in grid.neighbors(*pos):
+            end_elevation = resolve_elevation(grid[n])
+            if ord(end_elevation) - ord(start_elevation) > 1:
+                continue
+            yield n, 1
+
+    graph = lib.graph.make_lazy_graph(neighbor_fn)
+    coords_by_value = grid.coords_by_value()
+    start = coords_by_value['a'] + coords_by_value['S']
+    end = grid.find('E')
+    answer = lib.graph.dijkstra_length(graph, start, end)
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
