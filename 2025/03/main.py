220aa1bf99edb5aecfc86cbbeb4ba1620579902a
Ly Gia Lap <lap.ly@elca.vn>
2025-12-03 12:11:40 +0700

    2025 Day 3 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to decorating the North Pole. <a href="/2025/day/3#part2">[Continue to Part Two]</a></p></article>
    
    Submitted 16699 at 0:10:38.847586
    Submitted 17278 at 0:11:39.711536
    
    Please be a good citizen and remove answers from the commit message before pushing!

---
+++ 2025/03/main.py
@@ -0,0 +1,56 @@
+import collections
+import functools
+import itertools
+import json
+import math
+import parse
+import re2 as re
+import sympy # sympy.parse_expr, sympy.solve, sympy.Eq
+import sys # sys.setrecursionlimit(1000000)
+import z3 # x = z3.Int('x'); x < 0; (x-1) >= 0
+# z3.If(x >= 0, x, -x); z3.And(); z3.Or(); z3.Not()
+# s = z3.Solver(); solver.add(constraint); s.check(); s.model()[x].as_long()
+# o = z3.Optimize(); o.minimize(x); o.check(); o.model()[x].as_long()
+
+import lib.algorithms
+import lib.aoc
+import lib.cyk
+import lib.graph
+from lib.graphics import *
+import lib.grid
+import lib.hex_coord
+import lib.lazy_dict
+import lib.math
+import lib.ocr
+import lib.parsing
+
+def parse_input(s):
+    banks = s.splitlines()
+    return banks
+
+def max_two_digit(s: str) -> int:
+    digits = [int(c) for c in s]
+    best = -1
+    for i in range(len(digits)):
+        for j in range(i + 1, len(digits)):
+            best = max(best, digits[i] * 10 + digits[j])
+    return best
+
+def part1(s):
+    banks = parse_input(s)
+
+    answer = 0
+    for bank in banks:
+        answer += max_two_digit(bank)
+
+    lib.aoc.give_answer_current(1, answer)
+
+def part2(s):
+    pass
+    _ = parse_input(s)
+    answer = 0
+    # lib.aoc.give_answer_current(2, answer)
+
+INPUT = lib.aoc.get_current_input()
+part1(INPUT)
+part2(INPUT)
