87cab8155f1aba4abef553521d3d6bbb568bea53
Ly Gia Lap <lap.ly@elca.vn>
2025-12-03 08:58:59 +0700

    2022 Day 3 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit.</p><p>You have completed Day 3! You can <span class="share">[Share<span class="share-content">on

--- 2022/03/main.py
+++ 2022/03/main.py
@@ -26,10 +26,14 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
+    rucksacks = parse_input(s)
+    groups = [rucksacks[i:i+3] for i in range(0, len(rucksacks), 3)]
     answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    for group in groups:
+        items = [set(rucksack) for rucksack in group]
+        badge = list(functools.reduce(set.intersection, items))[0]
+        answer += priorities.index(badge)
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 # INPUT = '''vJrwpWtwJgWrhcsFMMfFFhFp
