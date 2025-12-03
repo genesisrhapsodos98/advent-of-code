26a3580c8c724ab2952064c058eaf0b26a3334d4
Ly Gia Lap <lap.ly@elca.vn>
2025-12-03 09:12:45 +0700

    2022 Day 4 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit.</p><p>You have completed Day 4! You can <span class="share">[Share<span class="share-content">on

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -19,18 +19,19 @@ def part1(s):
     pairs = parse_input(s)
     answer = 0
     for pair in pairs:
-        [l, r] = pair
-        sl, el = l
-        sr, er = r
+        (sl, el), (sr, er) = pair
         if (sl <= sr and el >= er) or (sr <= sl and er >= el):
             answer += 1
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
+    pairs = parse_input(s)
     answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    for pair in pairs:
+        (sl, el), (sr, er) = pair
+        if not (el < sr or er < sl):
+            answer += 1
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
