2ac860ee5241df57e108d4fb597f535e439fe1af
Ly Gia Lap <lap.ly@elca.vn>
2025-12-03 12:42:36 +0700

    2022 Day 9 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit.</p><p>You have completed Day 9! You can <span class="share">[Share<span class="share-content">on
    
    Submitted 2369 at 1090 days, 0:42:35.251099
    
    Please be a good citizen and remove answers from the commit message before pushing!

--- 2022/09/main.py
+++ 2022/09/main.py
@@ -63,10 +63,12 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
-    answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    motions = parse_input(s)
+    rope = head_positions(motions)
+    for _ in range(9):
+        rope = next_segment_positions(rope)
+    answer = len(set(rope))
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
