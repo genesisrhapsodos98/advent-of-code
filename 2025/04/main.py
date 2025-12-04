fb55740bebaee70dc4bb93e91f74d3611b9491f3
Ly Gia Lap <lap.ly@elca.vn>
2025-12-04 12:02:56 +0700

    2025 Day 4 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to decorating the North Pole.</p><p>You have completed Day 4! You can <span class="share">[Share<span class="share-content">on
    
    Submitted 9206 at 0:02:55.525012
    
    Please be a good citizen and remove answers from the commit message before pushing!

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -37,9 +37,22 @@ def part1(s):
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
-    answer = 0
+    grid = parse_input(s)
+    total_removed = 0
+    while True:
+        to_remove = [
+            (x, y)
+            for (x, y), v in grid.items()
+            if v == '@' and sum(grid[nx, ny] == '@' for nx, ny in grid.neighbors(x, y, diagonals=True)) < 4
+        ]
+        if not to_remove:
+            break
+        for x, y in to_remove:
+            grid[x, y] = '.'
+        total_removed += len(to_remove)
+    answer = total_removed
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
+part2(INPUT)
