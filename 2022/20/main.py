2422eb48ee42f7b965bfba4d80e5b5d98d791838
Genesis <genesisrhapsodos98@gmail.com>
2025-12-05 15:48:15 +0700

    2022 Day 20 Part 2
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit.</p><p>You have completed Day 20! You can <span class="share">[Share<span class="share-content">on
    
    Submitted 3033720253914 at 1081 days, 3:48:14.462830
    
    Please be a good citizen and remove answers from the commit message before pushing!

--- 2022/20/main.py
+++ 2022/20/main.py
@@ -27,26 +27,30 @@ import lib.parsing
 def parse_input(s):
     return list(map(int, s.splitlines()))
 
-def part1(s):
-    nums = parse_input(s)
 
-    # Use tuples (val, original_index) to handle duplicates
+def solve(s, decryption_key=1, rounds=1):
+    nums = parse_input(s)
+    nums = [x * decryption_key for x in nums]
+
+    # Use tuples (val, original_index) to handle duplicates and track original order
     mixed = [(x, i) for i, x in enumerate(nums)]
     original_order = list(mixed)
     N = len(mixed)
 
-    for item in original_order:
-        current_idx = mixed.index(item)
+    for _ in range(rounds):
+        for item in original_order:
+            current_idx = mixed.index(item)
 
-        # We remove the item, so the list size becomes N-1
-        mixed.pop(current_idx)
+            # Remove the item, effectively reducing list size to N-1
+            mixed.pop(current_idx)
 
-        val = item[0]
-        # Calculate new index in the list of size N-1
-        new_idx = (current_idx + val) % (N - 1)
+            val = item[0]
+            # Calculate new index in the list of size N-1
+            # The % operator in Python handles negative numbers correctly (wrapping around)
+            new_idx = (current_idx + val) % (N - 1)
 
-        # Insert the item back
-        mixed.insert(new_idx, item)
+            # Insert the item back at the new position
+            mixed.insert(new_idx, item)
 
     # Find the tuple with value 0
     zero_tuple = next(x for x in mixed if x[0] == 0)
@@ -56,14 +60,15 @@ def part1(s):
     c2 = mixed[(zero_idx + 2000) % N][0]
     c3 = mixed[(zero_idx + 3000) % N][0]
 
-    answer = c1 + c2 + c3
+    return c1 + c2 + c3
+
+def part1(s):
+    answer = solve(s, decryption_key=1, rounds=1)
     lib.aoc.give_answer_current(1, answer)
 
 def part2(s):
-    pass
-    _ = parse_input(s)
-    answer = 0
-    # lib.aoc.give_answer_current(2, answer)
+    answer = solve(s, decryption_key=811589153, rounds=10)
+    lib.aoc.give_answer_current(2, answer)
 
 INPUT = lib.aoc.get_current_input()
 part1(INPUT)
