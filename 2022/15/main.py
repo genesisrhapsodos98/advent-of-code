735c8278d7f9ea9668f556e22ff3acf700d98c01
Genesis <genesisrhapsodos98@gmail.com>
2025-12-05 08:54:30 +0700

    2022 Day 15 Part 1
    <article><p>That's the right answer!  You are <span class="day-success">one gold star</span> closer to collecting enough star fruit. <a href="/2022/day/15#part2">[Continue to Part Two]</a></p></article>
    
    Submitted 9378154 at 1085 days, 20:49:58.060226
    Submitted 5511201 at 1085 days, 20:54:29.159155
    
    Please be a good citizen and remove answers from the commit message before pushing!

---
+++ 2022/15/main.py
@@ -0,0 +1,76 @@
+import collections
+import functools
+import itertools
+import json
+import math
+import parse
+import re2 as re
+import sympy # sympy.parse_expr, sympy.solve, sympy.Eq
+import sys # sys.setrecursionlimit(1000000)
+import z3 # x = z3.Int('x'); x < 0; (x-1) >= 0
+# z3.If(x >= 0, x, -x); z3.And(); z3.Or(); z3.Not()
+# s = z3.Solver(); solver.add(constraint); s.check(); s.model()[x].as_long()
+# o = z3.Optimize(); o.minimize(x); o.check(); o.model()[x].as_long()
+
+import lib.algorithms
+import lib.aoc
+import lib.cyk
+import lib.graph
+from lib.graphics import *
+import lib.grid
+import lib.hex_coord
+import lib.lazy_dict
+import lib.math
+import lib.ocr
+import lib.parsing
+
+class Sensor:
+    def __init__(self, line):
+        parts = line.split()
+        self.position = tuple(map(int, (parts[2][2:-1], parts[3][2:-1])))
+        self.beacon = tuple(map(int, (parts[8][2:-1], parts[9][2:])))
+        self.scan_dist = sum(abs(p - b) for p, b in zip(self.position, self.beacon))
+
+def parse_input(s):
+    lines = s.splitlines()
+    sensors = list(map(Sensor, lines))
+    return sensors
+
+def part1(s):
+    sensors = parse_input(s)
+    TARGET_Y = 2_000_000
+
+    beacons_in_row = len(set(s.beacon[0] for s in sensors if s.beacon[1] == TARGET_Y))
+
+    visible_ranges = []
+    for s in sensors:
+        half_width = s.scan_dist - abs(s.position[1] - TARGET_Y)
+        if half_width < 0:
+            continue
+
+        visible_ranges.append((s.position[0] - half_width, s.position[0] + half_width))
+
+    visible_ranges.sort()
+
+    compact = []
+    low_x, high_x = visible_ranges[0]
+    for n_low_x, n_high_x in visible_ranges[1:]:
+        if n_low_x - 1 <= high_x:
+            high_x = max(high_x, n_high_x)
+        else:
+            compact.append((low_x, high_x))
+            low_x, high_x = n_low_x, n_high_x
+    compact.append((low_x, high_x))
+    answer = sum(high-low+1 for low, high in compact) - beacons_in_row
+
+    lib.aoc.give_answer_current(1, answer)
+
+def part2(s):
+    pass
+    _ = parse_input(s)
+    answer = 0
+    # lib.aoc.give_answer_current(2, answer)
+
+INPUT = lib.aoc.get_current_input()
+part1(INPUT)
+part2(INPUT)
