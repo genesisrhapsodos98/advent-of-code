978fcb59c562a79f00b15c876691ea9135e1b5de
Ly Gia Lap <lap.ly@elca.vn>
2025-12-11 12:21:18 +0700

    Improve 2025 Day 11

--- 2025/11/main.py
+++ 2025/11/main.py
@@ -1,28 +1,6 @@
 import collections
-import functools
-import itertools
-import json
-import math
-import parse
-import re2 as re
-import sympy # sympy.parse_expr, sympy.solve, sympy.Eq
-import sys # sys.setrecursionlimit(1000000)
-import z3 # x = z3.Int('x'); x < 0; (x-1) >= 0
-# z3.If(x >= 0, x, -x); z3.And(); z3.Or(); z3.Not()
-# s = z3.Solver(); solver.add(constraint); s.check(); s.model()[x].as_long()
-# o = z3.Optimize(); o.minimize(x); o.check(); o.model()[x].as_long()
 
-import lib.algorithms
 import lib.aoc
-import lib.cyk
-import lib.graph
-from lib.graphics import *
-import lib.grid
-import lib.hex_coord
-import lib.lazy_dict
-import lib.math
-import lib.ocr
-import lib.parsing
 
 def parse_input(s: str):
     graph = {}
@@ -41,31 +19,13 @@ def parse_input(s: str):
     return graph
 
 
-def count_paths(graph, start, end):
-    memo = {}
-
-    def dfs(node):
-        if node == end:
-            return 1
-        if node in memo:
-            return memo[node]
-        total = 0
-        for nxt in graph.get(node, []):
-            total += dfs(nxt)
-        memo[node] = total
-        return total
-
-    return dfs(start)
-
-
-def part1(s: str):
-    graph = parse_input(s)
-    answer = count_paths(graph, "you", "out")
-    lib.aoc.give_answer_current(1, answer)
-def count_paths_with_nodes(graph, start, end, must_visit):
+def count_paths(graph, start, end, must_visit=None):
+    if must_visit is None:
+        must_visit = []
     must_visit = set(must_visit)
     memo = {}
 
+    # DFS with state memoization using a bitmask to track all visited nodes in list of must_visit nodes
     def dfs(node, visited_mask):
         if node == end:
             return 1 if visited_mask == (1 << len(must_visit)) - 1 else 0
@@ -89,9 +49,15 @@ def count_paths_with_nodes(graph, start, end, must_visit):
     return dfs(start, 0)
 
 
+def part1(s: str):
+    graph = parse_input(s)
+    answer = count_paths(graph, "you", "out")
+    lib.aoc.give_answer_current(1, answer)
+
+
 def part2(s: str):
     graph = parse_input(s)
-    answer = count_paths_with_nodes(graph, "svr", "out", ["dac", "fft"])
+    answer = count_paths(graph, "svr", "out", ["dac", "fft"])
     lib.aoc.give_answer_current(2, answer)
     
 
